b0b0c7389781a72be8762b2aba8c665b8a947f24
austinhco <seven.pin.lightning@gmail.com>
2024-02-04 22:08:42 -0500

    Added password validation
    Passwords validated for requirements upon signup

--- incollege/services/AuthService.py
+++ incollege/services/AuthService.py
@@ -3,7 +3,14 @@
 
 import incollege.config.Config as Config
 import hashlib
+import re
 import incollege.repositories.AuthRepository as AuthRepository
+
+# This should ideally be on the user interface end, but that does not exist, so it's here
+def validate_password(password):
+    length_check = 8 <= len(password) <= 12
+    types_check = bool(re.search(r'(?=.*\d)(?=.*[^A-Za-z0-9])(?=.*[A-Z])', password))
+    return length_check and types_check
 
 def hash_password(password):
     return hashlib.sha512(str.encode(password+Config.SALT)).digest()
@@ -16,7 +23,7 @@ def login(username, password):
 
 def signup(username, password):
     if not username or not password or AuthRepository.user_exists(username) \
-            or AuthRepository.get_user_count() >= Config.USER_LIMIT:
+            or not validate_password(password) or AuthRepository.get_user_count() >= Config.USER_LIMIT:
         return False
     AuthRepository.create_user(username, hash_password(password))
     return True
