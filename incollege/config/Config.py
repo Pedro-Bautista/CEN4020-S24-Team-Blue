1e44df1aa43c6e6c7893d1e32d7744d2a87de18d
austinhco <seven.pin.lightning@gmail.com>
2024-02-05 13:28:22 -0500

    Added token to authentication
    Used PyJWT to return access tokens upon successful authentication

--- incollege/config/Config.py
+++ incollege/config/Config.py
@@ -2,5 +2,7 @@
 # Holds global configuration data
 
 DATABASE_NAME = 'InCollege.db'
-SALT = '1234'
+SECRET = '877e3401-8b72-4caf-ac7b-3f828c6b4987a19f2a72-e11a-42f4-adc4-65db302e95ea'
+SALT = 'M08k7wMIMKQwxryK7t922DUdg9HsmBK8'
+TOKEN_DURATION = 24
 USER_LIMIT = 5
