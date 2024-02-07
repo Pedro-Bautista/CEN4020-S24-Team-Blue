78644cd33268461c778709b9d9840fd70e827a71
Ye <zhaye125@gmail.com>
2024-02-06 19:03:10 -0500

    all test cases

--- tests/test_authController.py
+++ tests/test_authController.py
@@ -11,7 +11,6 @@ from incollege.repositories.DBConnector import create_tables
 """
 signup with valid user and pass
 signup with repeat username
-signup with repeat pass
 signup with empty user
 signup with empty pass
 signup with invalid pass (too long)
@@ -23,13 +22,6 @@ login with invalid pass
 login with empty user
 login with empty pass
 login with valid user and pass
-
-call Home
-call Jobs
-call People
-call Learn
-call Profile
-call Logout
 """
 
 @pytest.fixture
@@ -52,8 +44,9 @@ def client(app):
     with app.test_client() as client:
         yield client
 
+""" SIGNUP TESTS ---------------------------------------------------------------"""
 
-def test_signup(client):
+def test_signup_valid(client):
     # Create JSON data for signup request
     signup_data = {'username': 'student1', 'password': 'sp3ci@L8'}
 
@@ -66,6 +59,75 @@ def test_signup(client):
     # Check if the response contains a token
     assert 'token' in json.loads(response.data)
 
+def test_signup_repeatUser(client):
+
+    signup_data = {'username': 'student1', 'password': 'sp3ci@L9'}
+
+    response = client.post('/signup', json=signup_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+def test_signup_emptyUser(client):
+
+    signup_data = {'username': '', 'password': 'sp3ci@L9'}
+
+    response = client.post('/signup', json=signup_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+
+def test_signup_emptyPassword(client):
+
+    signup_data = {'username': 'student2', 'password': ''}
+
+    response = client.post('/signup', json=signup_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+
+def test_signup_longPassword(client):
+
+    signup_data = {'username': 'student2', 'password': 'toolongpPW@123'}
+
+    response = client.post('/signup', json=signup_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+
+def test_signup_partialPassword(client):
+
+    signup_data = {'username': 'student2', 'password': 'randomPW'}
+
+    response = client.post('/signup', json=signup_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+
+def test_signup_maxUsers(client):
+
+    for i in range(0,4):
+        signup_data = {'username': 'student'+str(i+2), 'password': 'sp3ci@L'+str(i+9)}
+
+        response = client.post('/signup', json=signup_data)
+
+        assert response.status_code == 200
+
+        assert 'token' in json.loads(response.data)
+
+    signup_data = {'username': 'maxStudent', 'password': 'maxUsers@12'}
+
+    response = client.post('/signup', json=signup_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+    
+""" LOGIN TESTS ---------------------------------------------------------------"""
 
 def test_login(client):
     # Create JSON data for login request
@@ -81,9 +143,49 @@ def test_login(client):
     assert 'token' in json.loads(response.data)
 
 
-def test_login_invalid(client):
 
-    login_data = {'username': 'student1', 'password': 'sp3ci@L9'}
+def test_login_invalidPass(client):
+
+    for i in range(0,4):
+        login_data = {'username': 'student'+str(i+1), 'password': 'sp3ci@L'+str(i+8)}
+
+        response = client.post('/login', json=login_data)
+
+        assert response.status_code == 200
+
+        assert 'token' in json.loads(response.data)
+
+    login_data = {'username': 'maxStudent', 'password': 'maxUsers@12'}
+
+    response = client.post('/login', json=login_data)
+    
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+
+def test_login_invalidUser(client):
+
+    login_data = {'username': 'student2', 'password': 'sp3ci@L8'}
+
+    response = client.post('/login', json=login_data)
+
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+
+
+def test_login_invalid_empty_user(client):
+
+    login_data = {'username': ' ', 'password': 'sp3ci@L9'}
+
+    response = client.post('/login', json=login_data)
+
+    # Check if the response status code is internal error (Unauthorized)
+    assert response.status_code == 500
+    
+
+def test_login_invalid_empty_pass(client):
+
+    login_data = {'username': 'student1', 'password': ''}
 
     response = client.post('/login', json=login_data)
 
