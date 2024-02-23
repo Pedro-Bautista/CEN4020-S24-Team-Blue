844923f7df117dbcc405e78b52073764a30d2ce3
austinhco <seven.pin.lightning@gmail.com>
2024-02-23 14:21:17 -0500

    Updated user limit to 10

--- incollege/config/Config.py
+++ incollege/config/Config.py
@@ -10,5 +10,5 @@ DATABASE_SCHEMA = project_folder + os.path.sep + 'schema.sql'
 SECRET = '877e3401-8b72-4caf-ac7b-3f828c6b4987a19f2a72-e11a-42f4-adc4-65db302e95ea'
 SALT = 'M08k7wMIMKQwxryK7t922DUdg9HsmBK8'
 TOKEN_DURATION = 24
-USER_LIMIT = 5
+USER_LIMIT = 10
 JOB_LIMIT = 5
