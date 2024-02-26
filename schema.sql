0a835a0920338590a0f45aeff5980471d7340e50
Pedro bautista <bautista17@usf.edu>
2024-02-25 22:54:50 -0800

    added major and univerisyt to the user Schema

--- schema.sql
+++ schema.sql
@@ -10,6 +10,8 @@ CREATE TABLE IF NOT EXISTS users (
     username TEXT,
     first_name TEXT,
     last_name TEXT,
+    university TEXT,
+    major TEXT,
     language_pref TEXT,
     email_pref TEXT,
     sms_pref TEXT,
