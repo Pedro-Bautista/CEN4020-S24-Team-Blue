ee4cc811082ac4f33171be1d744d4aeb347e1030
austinhco <seven.pin.lightning@gmail.com>
2024-03-02 13:17:13 -0500

    Fixed schema.sql primary key syntax

--- schema.sql
+++ schema.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS auth (
-    user_id TEXT PRIMARY KEY,
+    user_id TEXT,
     username TEXT,
     password_hash TEXT,
-    permissions_group TEXT
+    permissions_group TEXT,
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id TEXT PRIMARY KEY,
+    user_id TEXT,
     username TEXT,
     first_name TEXT,
     last_name TEXT,
@@ -15,25 +16,28 @@ CREATE TABLE IF NOT EXISTS users (
     language_pref TEXT,
     email_pref TEXT,
     sms_pref TEXT,
-    targeted_adv_pref TEXT
+    targeted_adv_pref TEXT,
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS jobs (
-    job_id TEXT PRIMARY KEY,
+    job_id TEXT,
     owner_user_id TEXT,
     title TEXT,
     desc TEXT,
     employer TEXT,
     location TEXT,
-    salary REAL
+    salary REAL,
+    PRIMARY KEY (job_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS connections (
-    sender_user_id TEXT PRIMARY KEY,
-    recipient_user_id TEXT PRIMARY KEY,
-    status TEXT DEFAULT 'pending',
+    sender_user_id TEXT,
+    recipient_user_id TEXT,
+    status TEXT DEFAULT 'PENDING',
     FOREIGN KEY (sender_user_id) REFERENCES users(user_id),
-    FOREIGN KEY (recipient_user_id) REFERENCES users(user_id)
+    FOREIGN KEY (recipient_user_id) REFERENCES users(user_id),
+    PRIMARY KEY (sender_user_id, recipient_user_id)
 );
 
