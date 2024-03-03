e15fed28a41d13a057f5fc15e950d760294e8f48
Pedro bautista <bautista17@usf.edu>
2024-03-03 16:46:27 -0500

    Updated shema and user.py to add bio,experience, and education field while initializing to an empty string.

--- incollege/entity/User.py
+++ incollege/entity/User.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, user_id, username, first_name, last_name, university='', major='', language_pref='english',
+    def __init__(self, user_id, username, first_name, last_name, university='', major='',bio='',experience='',education='', language_pref='english',
                  email_pref=1, sms_pref=1,
                  targeted_adv_pref=1):
         self.user_id = user_id
@@ -8,6 +8,9 @@ class User:
         self.last_name = last_name
         self.university = university
         self.major = major
+        self.bio=bio
+        self.experience = experience
+        self.education = education
         self.language_pref = language_pref
         self.email_pref = email_pref
         self.sms_pref = sms_pref
