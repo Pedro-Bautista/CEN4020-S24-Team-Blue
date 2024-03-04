dc6dec4833564dee6b8ff2ae09159219cc1876b3
Pedro bautista <bautista17@usf.edu>
2024-03-03 21:29:36 -0500

    Initial Stages of Profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,13 +1,40 @@
-import { AuthData } from "../auth/AuthWrapper"
+import { AuthData } from "../auth/AuthWrapper";
+import React, { useEffect, useState } from "react";
+import api from "../api/api";
 
 export const Profile = () => {
+  const { user } = AuthData();
+  const [userData, setUserData] = useState([]);
 
-	const { user } = AuthData()
+  useEffect(() => {
+    const handleUserData = async () => {
+      try {
+        const responseData = await api.getUserData({});
+        console.log("User Data", responseData);
+        setUserData(responseData.message);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
 
-	return (
-		<div className="page">
-			<h2>Your Profile</h2>
-			<p>Username: {user.username}</p>
-		</div>
-	)
+    // Call handleUserData when the component mounts
+    handleUserData();
+  }, []); // Empty dependency array ensures it runs only once when the component mounts
+
+  return (
+    <div className="page">
+      <h2>Your Profile</h2>
+      <div>
+        <h2>BYE</h2>
+        {userData.map((data, index) => (
+          <div key={index} className="profile-summary">
+            <h2>HEY</h2>
+            <div className={"Basic data"}>
+				{`${data.first_name} ${data.last_name}`}<br/>
+			</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
