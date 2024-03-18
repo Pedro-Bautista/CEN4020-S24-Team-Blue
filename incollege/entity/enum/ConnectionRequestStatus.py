4888777468277bdb4d7c2ef8879f7a30f019a8f1
austinhco <austinholmes@usf.edu>
2024-03-18 13:40:55 -0400

    Added entity documentation
    Documented all entity classes less enums

--- incollege/entity/enum/ConnectionRequestStatus.py
+++ incollege/entity/enum/ConnectionRequestStatus.py
@@ -1,19 +1,20 @@
 from incollege.exceptions.ContentException import ContentException
 
 
+def from_string(status_string: str) -> int:
+    upper_status = status_string.upper()
+    match upper_status:
+        case 'PENDING':
+            return ConnectionRequestStatus.PENDING
+        case 'ACCEPTED':
+            return ConnectionRequestStatus.ACCEPTED
+        case 'REJECTED':
+            return ConnectionRequestStatus.REJECTED
+        case _:
+            raise ContentException('No such connection request status.', 400)
+
+
 class ConnectionRequestStatus:
     PENDING = 0
     ACCEPTED = 1
     REJECTED = 2
-
-    def from_string(self, status_string):
-        upper_status = status_string.upper()
-        match upper_status:
-            case 'PENDING':
-                return 0
-            case 'ACCEPTED':
-                return 1
-            case 'REJECTED':
-                return 2
-            case _:
-                raise ContentException('No such connection request status.', 400)
